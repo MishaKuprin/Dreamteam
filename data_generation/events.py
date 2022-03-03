d8af877ca162a8bed4456a769e9a8a389b049cea
MishaKuprin <100375921+MishaKuprin@users.noreply.github.com>
2022-03-03 16:32:10 +0300

    vers 3

--- data_generation/events.py
+++ data_generation/events.py
@@ -5,13 +5,18 @@ import pandas as pd
 
 customer_Data = pd.read_csv('Customer.csv',index_col=False)
 fake = Faker()
-#print(type(fake.date_time_between_dates(datetime_start=datetime.datetime(2020,1,1,6,1,1), datetime_end=datetime.datetime(2020,1,1,7,1,1))))
+
+def Customer_ID_for_product_instanse(customer_Data):
+    customer_ID = customer_Data.ID
+    return  customer_ID
+
 def date_of_event():
     dateEvent = []
+    timeEvent = []
     events_per_day = np.random.randint(1,20, size=10)
     for i in range(len(events_per_day)):
         for j in range(events_per_day[i]*600):
             dateEvent.append(fake.date_between(start_date=datetime.date(2018,1,1)))
-    return dateEvent
+            timeEvent.append(fake.time())
+    return dateEvent,timeEvent
 
-date_of_event()
