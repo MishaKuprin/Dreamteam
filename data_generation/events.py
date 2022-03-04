5f70c33586aafffae2b350e0857622a00da1b4bd
MishaKuprin <100375921+MishaKuprin@users.noreply.github.com>
2022-03-04 21:09:45 +0300

    vers 6

--- data_generation/events.py
+++ data_generation/events.py
@@ -12,16 +12,6 @@ def business_product_instance_id_gen(product_instance_Data):
     business_product_instance_id_for = product_instance_Data.business_product_instance_id
     return business_product_instance_id_for
 
-def date_of_event_gen():
-    dateEvent = []
-    timeEvent = []
-    events_per_day = np.random.randint(1,20, size=10)
-    for i in range(len(events_per_day)):
-        for j in range(events_per_day[i]*600):
-            dateEvent.append(fake.date_between(start_date=datetime.date(2018,1,1)))
-            timeEvent.append(fake.time())
-    return dateEvent,timeEvent
-
 def format_time_transoform(product_instance_Data_test):
 
     product_instance_Data_test.termination_date.fillna(datetime.date.today(),inplace=True)
@@ -38,21 +28,31 @@ def format_time_transoform(product_instance_Data_test):
                                                                               int(date_split[1]),
                                                                               int(date_split[2]))
     all_days = []
+    a = []
+    b = []
     for i in range(len(product_instance_Data_test['activation_date'])):
         x = product_instance_Data_test.termination_date.values[i]-product_instance_Data_test.activation_date.values[i]
         x1 = int(x.days)
         all_days.append(x1)
-    print(all_days)
+        a.append(product_instance_Data_test.termination_date.values[i])
+        b.append(product_instance_Data_test.activation_date.values[i])
+    return all_days,a,b
 
 
+def date_of_event_gen(all_days,a,b):
+    dateEvent = []
+    timeEvent = []
+    business_product_instance_id = []
+    for i in range(len(all_days)):
+        for j in range(all_days[i]):
+            business_product_instance_id.append(i)
+            dateEvent.append(fake.date_between(start_date=b[i],end_date=a[i]))
+            timeEvent.append(fake.time())
+    return business_product_instance_id, dateEvent, timeEvent
 
 
-
-
-
-#product_instance_Data_test['Amount_of_time'] = product_instance_Data_test['termination_date'] - product_instance_Data_test['activation_date']
-
-#x = datetime.date(2022,2,2)-datetime.date(2022,2,1)
-#x1 = int(x.days)
-format_time_transoform(product_instance_Data_test)
-
+all_days,a,b = format_time_transoform(product_instance_Data_test)
+c,c1,c2 = date_of_event_gen(all_days,a,b)
+print(c)
+print(c1)
+print(c2)
