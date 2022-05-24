6803b811f95fdf7594272e7d1496a1681b8e4584
Eugene Skoroded <98937510+eugeneskoroded@users.noreply.github.com>
2022-05-24 21:04:02 +0300

    Delete airflow_jp_telecom directory

--- airflow_jp_telecom/scripts/scheduler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-eval "sudo service postgresql start"
-eval "cd $AIRFLOW_HOME\r"
-sh "./ubuntu_airflow_venv/bin/activate"
-eval "airflow scheduler"
--- airflow_jp_telecom/scripts/webserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-eval "cd $AIRFLOW_HOME"
-eval ". ubuntu_airflow_venv/bin/activate"
-eval "airflow webserver"
